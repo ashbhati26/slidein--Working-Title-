b662326fe1f28e20db9e7ce4ac72690ac48e3093
Ashish Bhati <ashbhati26@gmail.com>
2026-04-11 17:07:15 +0530

    privacy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Privacy Policy</h1>
+      <p>This app is under development and does not misuse user data.</p>
+    </div>
+  );
+}
